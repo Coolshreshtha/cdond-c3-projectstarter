bda5b7bad4d3dc96d61d69bf178b2919eb3959d4
Coolshreshtha <kulshres@grinnell.edu>
2021-11-03 13:07:44 -0500

    fix front end and back end handlers

--- frontend/src/app/components/LoadingMessage/LoadingMessage.spec.tsx
+++ frontend/src/app/components/LoadingMessage/LoadingMessage.spec.tsx
@@ -3,13 +3,13 @@ import { shallow } from 'enzyme';
 import { LoadingMessage } from 'app/components/LoadingMessage';
 
 describe('<LoadingMessage>', () => {
-  describe('Props', () => {
-    describe('message', () => {
-      it('Should render the props message', () => {
-        const message = 'Hello!';
-        const wrapper = shallow(<LoadingMessage message={message} />);
-        expect(wrapper.contains(<span>{message}?</span>)).toBeTruthy(); //remove the question mark to make the test pass
-      });
+    describe('Props', () => {
+        describe('message', () => {
+            it('Should render the props message', () => {
+                const message = 'Hello!';
+                const wrapper = shallow(<LoadingMessage message={message} />);
+                expect(wrapper.contains(<span>{message}</span>)).toBeTruthy(); //remove the question mark to make the test pass
+            });
+        });
     });
-  });
 });
